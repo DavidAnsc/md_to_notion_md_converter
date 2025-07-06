06c3a01b6cd673411622bf6b08902f2831758026
David An <ansichen.david@gmail.com>
2025-07-06 15:08:09 +0800

    [feature] achieve finding whole code

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import os
 import re
 continueLoop = True
-pattern = r"\b!\w+]"
+pattern = r"\w+ \d+-\d+-\d+ \w+ \d+.\d+.\d+ \w+.\w+]]"
+imagesFilePath = "/Users/david_an/Pictures/Others/Archive"
+
 while continueLoop:
-    initialMdPath= input("pls enter the relative file path of the .md file here: ")
+    initialMdPath= input("pls enter the file path of the .md file here: ")
+    initialMdPath = initialMdPath.replace("\\", "")
 
     if os.path.isfile(initialMdPath):
         print("the file path found successfully!")
@@ -13,10 +16,11 @@ while continueLoop:
         continue
 
     try:
-        with open(initialMdPath, "r") as file:
+        with open(initialMdPath, "w") as file:
             content = file.read()
             result = re.findall(pattern=pattern, string=content)
-        print(result.count)
+
+
     except:
         print("error!")
         break
