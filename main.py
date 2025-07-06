e43cd9d8d14651806dcb95e7d48f7c5a8fec3b1f
David An <ansichen.david@gmail.com>
2025-07-06 14:24:33 +0800

    [feature] test findall functionality

--- main.py
+++ main.py
@@ -1,4 +1,22 @@
 import os
 import re
+continueLoop = True
+pattern = r"\b!\w+]"
+while continueLoop:
+    initialMdPath= input("pls enter the relative file path of the .md file here: ")
 
-initialFilePath= input("pls enter the relative file path here: ")
+    if os.path.isfile(initialMdPath):
+        print("the file path found successfully!")
+    else:
+        print("sorry, you entered a wrong file path.")
+        print("pls try again!")
+        continue
+
+    try:
+        with open(initialMdPath, "r") as file:
+            content = file.read()
+            result = re.findall(pattern=pattern, string=content)
+        print(result.count)
+    except:
+        print("error!")
+        break
