117d650593a2405cb196f086820fb75a1ba5544a
David An <ansichen.david@gmail.com>
2025-11-27 20:59:40 -0500

    [feature] big improvements

--- main.py
+++ main.py
@@ -47,7 +47,48 @@ while menu_selection:
 
     if answer == "1":
         menu_selection = False
-        # TODO: write the user manual here
+
+        print("\n**\nwelcome to our user manual\n**\n")
+        print("""
+        This is a program that can turn obsidian .md files to notion-readable .md files.
+        
+        You'll need:
+        - a folder contains all images that you used in the .md file
+        - the obsidian .md file
+
+        Here's the procedure guys:
+
+        ## PREPARATION
+
+        In this program, all you have to prepare is a .md text file 
+        which is the document that contains image hyperlinks
+        or other formats that only work for Obsidian so need format conversion, 
+        and a folder with the images that were used in the document.
+        
+        *
+        (You don't actually need the folder with images, 
+        you'll only need it before importing to Notion or other softwares.)
+        *
+
+        ## CONVERSION
+
+        After finishing the preparation, you can go to "2" and start the conversion,
+        it will produce a file called "outputMd.md" file into a folder called "output"
+        in the project directory.
+
+        ## IMPORT
+
+        Now, we need to import, to import to Notion, we're going to produce a .zip file.
+        First, copy the folder with the image source files into the same directory as the
+        .md file that generated. ** Next, rename the folder to "Archive" (this step 
+        is important). ** Finally, select "outputMd.md" and the "Archive" folder,
+        compress them into one .zip file and import it into Notion.
+
+        (It might take longer than expected if you have a large Archive folder.)
+
+        """)
+
+        print("\n**\nuser manual ends\n**\n")
 
 
 
@@ -92,7 +133,7 @@ while menu_selection:
                     print("**\n** error while trying to read the file from the given file path. **\n**")
                     break
 
-                exportFilePath = "../Outputs/outputMd.md"
+                exportFilePath = "outputs/outputMd.md"
 
                 newResult2: list[str] = []
 
@@ -133,7 +174,7 @@ while menu_selection:
 
     elif answer == "3":
         menu_selection = True
-        print("\n**\nhttps://github.com/DavidAnsc/md_to_notion_md_converter\n**\n")
+        print("\n**  link:\nhttps://github.com/DavidAnsc/md_to_notion_md_converter\n**\n")
         # get the link here:
 
 
